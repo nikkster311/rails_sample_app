6a68099c5ad0a6d06b194356c0bcfa44bb078b6d
Nikki <nikkster311@gmail.com>
2022-01-12 07:45:08 +0000

    added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # maps requests for url to home action in static pages controller
   get 'static_pages/home'
   get 'static_pages/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
